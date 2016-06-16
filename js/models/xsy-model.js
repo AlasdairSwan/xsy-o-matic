27c461f524fbcd5de74a2794e8c30c74802390b2
AlasdairSwan <aswan@edx.org>
2016-06-16 14:32:27 -0400

    Added the mouth and eyes

--- js/models/xsy-model.js
+++ js/models/xsy-model.js
@@ -7,7 +7,13 @@ define([
         return Backbone.Model.extend({
             defaults: {
                 bodyColor: '#73bde7',
-                armColor: '#73bde7'
+                armColor: '#73bde7',
+                eyeballColor: '#fff',
+                eyeLinerColor: '#000',
+                retinaColor: '#000',
+                lips: '#73bde7',
+                teeth: '#fff',
+                tongue: '#f00'
             }
         });
     }
